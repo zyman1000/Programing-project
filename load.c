a2eae1cf70bf3838b17cffc4afd546cce993529c
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2024-12-26 12:02:48 +0200

    Update load.c

--- load.c
+++ load.c
@@ -22,35 +22,19 @@ typedef struct
   int price;
   int size;
 } Reservation;
-Reservation *Load()
-{
-  Reservation *Rooms = malloc(0);
-  char buffer[256];
-  char empty[256];
-  for(int i = 0; i < 256; i++){
-    empty[i] = ' ';
-  }
-  int j = 0, i = 1;
-  FILE *reservation = fopen("tests/Reservation.txt", "r");
-  // FILE *rooms = fopen("tests/Room.txt", "r");
-  if (reservation == NULL)
+Reservation *Load(){
+    char buffer[256];
+    Reservation *Rooms = malloc(0);
+    int j=0, i=1;
+    FILE *reservation = fopen ("Reservation.txt", "r"); 
+    FILE *rooms = fopen ("room.txt", "r");
+    if (reservation == NULL || rooms == NULL)
   {
-    printf("Error reading reservation file");
+    printf ("Error reading reservation file");
     exit(1);
   }
-  // if (rooms == NULL)
-  // {
-    // printf("Error reading reservation file");
-    // exit(1);
-  // }
-
-  while (!feof(reservation))
-  {
-    if(fgets(buffer, 256, reservation) == NULL){
-      break;
-    }
-    printf("%s", buffer);
-    Rooms = realloc(Rooms, sizeof(Reservation) * (i));
+  while(fgets(buffer, 256, reservation)){
+    Rooms = realloc(Rooms, sizeof(Reservation) * i);
     Rooms[j].reservation_ID = atoi(strtok(buffer, ","));
     Rooms[j].room_num = atoi(strtok(NULL, ","));
     strcpy(Rooms[j].status, strtok(NULL, ","));
@@ -61,45 +45,28 @@ Reservation *Load()
     strcpy(Rooms[j].check_in.month, strtok(NULL, ",-"));
     strcpy(Rooms[j].check_in.year, strtok(NULL, ",-"));
     strcpy(Rooms[j].email, strtok(NULL, ","));
-    strcpy(Rooms[j].phone_num, strtok(NULL, ","));
-    i += 1;
+    strcpy(Rooms[j].phone_num, strtok(NULL, ",\n"));
+    i++;
     j++;
-    strcpy(buffer, empty);
   }
-  Rooms[0].size = i - 1;
+  Rooms[0].size = i-1;
   j = 0;
-  // while (fgets(buffer, 256, rooms))
-  // {
-  //   if (atoi(strtok(buffer, " ")) == Rooms[j].room_num)
-  //   {
-  //     strtok(NULL, " ");
-  //     strcpy(Rooms[j].cat, strtok(NULL, " "));
-  //     Rooms[j].price = atoi(strtok(NULL, " "));
-  //     j++;
-  //   }
-  // }
+  while(fgets(buffer, 256, rooms)){
+    if (atoi(strtok(buffer, " ")) == Rooms[j].room_num){
+      strtok(NULL, " ");
+      strcpy(Rooms[j].cat, strtok(NULL, " "));
+      Rooms[j].price = atoi(strtok(NULL, " "));
+      j++;
+    }
+  }
+  
+
+  fclose(rooms);
   fclose(reservation);
-  // fclose(rooms);
   return Rooms;
-}
+  }
 
 int main() {
-  Reservation *please = Load();
-  for(int i = 0; i < 2; i++){
-    printf("%d  ", please[i].reservation_ID);
-    printf("%d  ", please[i].room_num);
-    printf("%s  ", please[i].status);
-    printf("%s  ", please[i].name);
-    printf("%s  ", please[i].ID);
-    printf("%d  ", please[i].nights_num);
-    printf("%s  ", please[i].check_in.day);
-    printf("%s  ", please[i].check_in.month);
-    printf("%s  ", please[i].check_in.year);
-    printf("%s  ", please[i].email);
-    printf("%s  ", please[i].phone_num);
-    printf("%s  ", please[i].cat);
-    printf("%d  ", please[i].price);
-    printf("%d  ", please[i].size);
-    printf("\n");
-  }
+  Reservation *Rooms = Load();
+  free(Rooms);
 }
